5d45b99a5c93f992ea47f1f57a5489b62cc405e2
yi-luo <42324106+yi-luo@users.noreply.github.com>
2019-11-08 11:30:23 +0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,72 +1,17 @@
-# This activity requires Python 3.7 runtime
-# -*- coding: utf-8 -*-
 import json
-import traceback
-import doctest
-
+import pandas as pd
+import boto3 as bt3
 import re
 
-import signal
-import time
-
-    
+status_check = [0]*10    
 def lambda_handler(event, context):
     
-    def run_local(requestDict):
-      byteCodeChecker = "\ndef byteCodeChecker(function, field, value):\n import dis\n byteCode = dis.Bytecode(function)\n fields = {'OPNAME':0, 'ARGREPR':3} \n for instruction in byteCode:\n  if instruction[fields[field]] == value:\n   return f'found {value}'\n else:\n  return f'{value} not found'"
-      codeInfoChecker = "\ndef codeInfoChecker(function, value):\n import dis\n codeInfo = dis.code_info(function)\n if value in codeInfo:\n  return f'found {value}'\n else:\n  return f'{value} not found'"
-      solution = requestDict['solution'] + codeInfoChecker + byteCodeChecker
-      tests = requestDict['tests']  
-      import io
-      import sys
-      output = io.StringIO()
-      sys.stdout = output
-      
-      try:
-        namespace = {}
-        compiled = compile('import json', 'submitted code', 'exec')
-        exec(compiled, namespace)
-        compiled = compile(solution, 'submitted code', 'exec')
-        exec(compiled, namespace)
-        namespace['YOUR_SOLUTION'] = solution.strip()
-        namespace['LINES_IN_YOUR_SOLUTION'] = len(solution.strip().splitlines())
-        test_cases = doctest.DocTestParser().get_examples(tests)
-        execute_test_cases(test_cases, namespace)
-        results, solved = execute_test_cases(test_cases, namespace)
-        printed = output.getvalue()
-        responseDict = {"solved": solved , "results": results, "printed":printed}
-        responseJSON = json.dumps(responseDict)
-        return responseJSON
-      except:
-        errors = traceback.format_exc()
-        responseDict = {'errors': '%s' % errors}
-        responseJSON = json.dumps(responseDict)
-        return responseJSON
-    
-    def execute_test_cases(testCases, namespace):
-      resultList = []
-      solved = True
-      for e in testCases:
-        if not e.want:
-          exec(e.source) in namespace
-          continue
-        call = e.source.strip()
-        got = eval(call, namespace)
-        expected = eval(e.want, namespace)
-        correct = True
-        if got == expected:
-          correct = True
-        else:
-          correct = solved = False
-        resultDict = {'call': call, 'expected': expected, 'received': "%(got)s" % {'got': got}, 'correct': correct}
-        resultList.append(resultDict)
-      return resultList, solved
-    
+    s3 = bt3.client('s3')
     method = event.get('httpMethod',{}) 
     
     with open('index.html', 'r') as f:
         indexPage = f.read()
-        
+    
     
     if method == 'GET':
         return {
@@ -78,128 +23,111 @@ def lambda_handler(event, context):
         }
         
     if method == 'POST':
-        import re
         bodyContent = event.get('body',{}) 
         parsedBodyContent = json.loads(bodyContent)
         testCases = re.sub('&zwnj;.*&zwnj;','',parsedBodyContent["shown"]["0"], flags=re.DOTALL) 
-        solution = parsedBodyContent["editable"]["0"] 
-
-        timeout = False
-        # handler function that tell the signal module to execute
-        # our own function when SIGALRM signal received.
-        def timeout_handler(num, stack):
-            print("Received SIGALRM")
-            raise Exception("processTooLong")
-
-        # register this with the SIGALRM signal    
-        signal.signal(signal.SIGALRM, timeout_handler)
+        userSolution = parsedBodyContent["editable"]["0"] 
+        questionName = parsedBodyContent["qname"]["0"]
         
-        # signal.alarm(10) tells the OS to send a SIGALRM after 10 seconds from this point onwards.
-        signal.alarm(10)
-
-        # After setting the alarm clock we invoke the long running function.
-        try:
-            jsonResponse = run_local({"solution": solution, "tests": testCases})
-        except Exception as ex:
-            if "processTooLong" in ex:
-                timeout = True
-                print("processTooLong triggered")
-        # set the alarm to 0 seconds after all is done
-        finally:
-            signal.alarm(0)
-
-        jsonResponseData = json.loads(jsonResponse)
+        # Reading the original dataframe from S3 
+        original_df = pd.read_csv('https://frame-pandas.s3.amazonaws.com/pandas_data.csv')
+        default_df = original_df.copy()
         
-        solvedStatusText = expectedText = receivedText = callText = textResults = tableContents = ""
-        overallResults = """<span class="md-subheading">All tests passed: {0}</span><br/>""".format(str(jsonResponseData.get("solved")))
-        numTestCases = len(re.findall('>>>', testCases))
-        resultContent = jsonResponseData.get('results') 
-        textBackgroundColor = "#ffffff"
-        
-        if resultContent:
-            for i in range(len(resultContent)):
-                expectedText = resultContent[i]["expected"]
-                receivedText = resultContent[i]["received"]
-                correctText = resultContent[i]["correct"]
-                callText = resultContent[i]["call"]
-                if str(expectedText) == str(receivedText):
-                    textResults = textResults + "\nHurray! You have passed the test case. You called {0} and received {1} against the expected value of {2}.\n".format(callText, receivedText, expectedText)
-                    textBackgroundColor = "#b2d8b2"
-                else:
-                    textResults = textResults + "\nThe test case eludes your code so far but try again! You called {0} and received {1} against the expected value of {2}.\n".format(callText, receivedText, expectedText)
-                    textBackgroundColor = "#ff9999"
-                tableContents = tableContents + """
-                <tr bgcolor={4}>
-                    <td>{0}</td>
-                    <td>{1}</td>
-                    <td>{2}</td>
-                    <td>{3}</td>
-                </tr>
-                """.format(callText,expectedText,receivedText,correctText,textBackgroundColor)
-        solvedStatusText = str(jsonResponseData.get("solved")) or "error"
-        textResults = """All tests passed: {0}\n""".format(solvedStatusText) + textResults
-        if not resultContent:
-            textResults = "Your test is passing but something is incorrect..."
+        # Evaluating User Inputs
+        user_list = userSolution.splitlines()
+        user_list = [x for x in user_list if not x.startswith('#')]
+        for i in user_list:
+            original_df = pd.eval(i)
             
-        if timeout or jsonResponseData.get("errors"):
-            textResults = "An error - probably related to code syntax - has occured. Do look through the JSON results to understand the cause."
-            tableContents = """
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td>error</td>
-                    <td></td>
-                </tr>
-                """
-        htmlResults="""
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                    <meta content="width=device-width,initial-scale=1,minimal-ui" name="viewport">
-                </head>
-                <body>
-                    <div>
-                        {0}
-                        <span class="md-subheading tableTitle">Tests</span>
-                        <table>
-                             <thead>
-                                <tr>
-                                    <th>Called</th>
-                                    <th>Expected</th>
-                                    <th>Received</th>
-                                    <th>Correct</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {1}
-                            </tbody>
-                        </table>
-                    </div>
-                </body>
-                <style>
-                br {{
-                    display:block;
-                    content:"";
-                    margin:1rem
-                }}
-                table{{
-                    text-align:center
-                }}
-                .tableTitle{{
-                    text-decoration:underline
-                }}
-                </style>
-            </html>
-            """.format(overallResults,tableContents)
+        print("THIS IS STRING", original_df)
+        
+        if isinstance(original_df, str):
+            userHtmlFeedback = original_df
+        elif isinstance(original_df, pd.core.series.Series):
+           original_df = original_df.to_frame()
+           userHtmlFeedback = original_df.to_html()
+        else:
+            userHtmlFeedback = original_df.to_html()
+        
+        right_answer_text = "temp"
+        isComplete = 0
+        
+        
+        if questionName == 'Selecting Rows':#Q1
+            right_answer = default_df.iloc[[1,4,9]]
+            right_answer_text = 'original_df.iloc[[1,4,9]]'
+            if(right_answer.equals(original_df)):
+                status_check[0]=1
+                isComplete = 1
+        elif questionName == 'Selecting Columns':#Q2
+            right_answer = default_df[['ID','NAME','RESIDENCE']]
+            right_answer_text = 'original_df[[\'ID\',\'NAME\',\'RESIDENCE\']]'
+            if(right_answer.equals(original_df)):
+                status_check[1]=1
+                isComplete = 1
+        elif questionName == 'Selecting Specific Cells':#Q3
+            right_answer = default_df.iloc[8]['NAME']
+            right_answer_text = 'original_df.iloc[8][\'NAME\']'
+            if(right_answer==(original_df)):
+                status_check[2]=1
+                isComplete = 1
+        elif questionName == 'Replacing String Occurrences':#Q4
+            right_answer = default_df.replace("USA","United States")
+            right_answer_text = 'original_df.replace("USA","United States")'
+            if(right_answer.equals(original_df)):
+                status_check[3]=1
+                isComplete = 1
+        elif questionName == 'Filtering Data in Columns':#Q5
+            right_answer = default_df[original_df['CHILDREN']=='Yes']
+            right_answer_text = 'original_df[original_df[\'CHILDREN\']==\'Yes\']'
+            if(right_answer.equals(original_df)):
+                status_check[4]=1
+                isComplete = 1
+        elif questionName == 'Filtering Data based on Multiple Conditions':#Q6
+            right_answer = default_df[(original_df['CHILDREN']=='Yes')|(original_df['RESIDENCE']=='China')]
+            right_answer_text = 'original_df[(original_df[\'CHILDREN\']==\'Yes\')|(original_df[\'RESIDENCE\']==\'China\')]'
+            if(right_answer.equals(original_df)):
+                status_check[5]=1
+                isComplete = 1
+        elif questionName == 'Adding Rows':#Q7
+            right_answer = default_df.append(pd.Series([11,'Tao Tao', 'M', 20, 'Yes', 'Singapore'], index=original_df.columns), ignore_index=True)
+            right_answer_text = 'original_df.append(pd.Series([11,\'Tao Tao\', \'M\', 20, \'Yes\', \'Singapore\'], index=original_df.columns), ignore_index=True)'
+            if(right_answer.equals(original_df)):
+                status_check[6]=1
+                isComplete = 1
+        elif questionName == 'Deleting Rows':#Q8
+            right_answer = default_df.drop(original_df.index[[4,9]])
+            right_answer_text = 'original_df.drop(original_df.index[[4,9]])'
+            if(right_answer.equals(original_df)):
+                status_check[7]=1
+                isComplete = 1
+        elif questionName == 'Finding Min and Max':#Q9
+            right_answer = default_df['NAME'].min()
+            right_answer_text = 'original_df[\'NAME\'].min()'
+            if(right_answer.equals(original_df)):
+                status_check[8]=1
+                isComplete = 1
+        elif questionName == 'Multiplying and Dividing Column Values':#Q10
+            right_answer = 10*default_df.iloc[:,3]
+            right_answer_text = '10*original_df.iloc[:,3]'
+            if(right_answer.equals(original_df)):
+                status_check[9]=1
+                isComplete = 1
+        
+        progress = status_check.count(1)
+        print(status_check)
+        print(progress)
         return {
             "statusCode": 200,
             "headers": {
             "Content-Type": "application/json",
                 },
             "body":  json.dumps({
-                "isComplete":jsonResponseData.get("solved"),
-                "jsonFeedback": jsonResponse,
-                "htmlFeedback": htmlResults,
-                "textFeedback": textResults
+                "isComplete":isComplete,
+                "pythonFeedback": "Hello",
+                "htmlFeedback": userHtmlFeedback,
+                "textFeedback": right_answer_text,
+                "progress": progress,
+                "questionStatus":status_check
             })
-            }
+        }
