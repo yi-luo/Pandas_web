600b3808011dfe8ae752adf94e7a659fafbcd927
yi-luo <42324106+yi-luo@users.noreply.github.com>
2019-11-11 19:32:43 +0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -59,9 +59,9 @@ def lambda_handler(event, context):
         # Reading the original dataframe from S3 
         original_df = pd.read_csv('https://frame-pandas.s3.amazonaws.com/pandas_data.csv')
         default_df = original_df.copy()
-        expected_output = ""
         
         errorStatus = False
+        expected_output = ""
         # Evaluating User Inputs
         try:
             expected_output = runCode(original_df, userSolution)
@@ -91,66 +91,66 @@ def lambda_handler(event, context):
             if(right_answer.equals(expected_output)):
                 status_check[0]=1
                 isComplete = 1
+        elif questionName == 'Selecting First/Last Rows':#Q10
+            right_answer = default_df.tail(3)
+            right_answer_text = '10*expected_output.tail(3)'
+            if(right_answer.equals(expected_output)):
+                status_check[1]=1
+                isComplete = 1
         elif questionName == 'Selecting Columns':#Q2
             right_answer = default_df[['ID','NAME','RESIDENCE']]
             right_answer_text = 'expected_output[[\'ID\',\'NAME\',\'RESIDENCE\']]'
             if(right_answer.equals(expected_output)):
-                status_check[1]=1
+                status_check[2]=1
                 isComplete = 1
         elif questionName == 'Selecting Specific Cells':#Q3
             right_answer = default_df.iloc[8]['NAME']
             right_answer_text = 'expected_output.iloc[8][\'NAME\']'
             if(right_answer==(expected_output)):
-                status_check[2]=1
+                status_check[3]=1
                 isComplete = 1
         elif questionName == 'Replacing String Occurrences':#Q4
             right_answer = default_df.replace(["F","M"],["Female","Male"])
             right_answer_text = 'expected_output.replace("USA","United States")'
             if(right_answer.equals(expected_output)):
-                status_check[3]=1
+                status_check[4]=1
                 isComplete = 1
         elif questionName == 'Filtering Data in Columns':#Q5
             right_answer = default_df[default_df['CHILDREN']=='Yes']
             right_answer_text = 'expected_output[original_df[\'CHILDREN\']==\'Yes\']'
             if(right_answer.equals(expected_output)):
-                status_check[4]=1
+                status_check[5]=1
                 isComplete = 1
         elif questionName == 'Filtering Data based on Multiple Conditions':#Q6
             right_answer = default_df[(default_df['RESIDENCE']=='China')|(default_df['AGE']>15)]
             right_answer_text = 'expected_output[(original_df[\'RESIDENCE\']==\'China\')|(original_df[\'AGE\']>\'15\')]'
             if(right_answer.equals(expected_output)):
-                status_check[5]=1
-                isComplete = 1
-        elif questionName == 'Adding Rows':#Q7
-            right_answer = default_df.append(pd.Series([11,'Tao Tao', 'M', 20, 'Yes', 'Singapore'], index=original_df.columns), ignore_index=True)
-            right_answer_text = 'expected_output.append(pd.Series([11,\'Tao Tao\', \'M\', 20, \'Yes\', \'Singapore\'], index=original_df.columns), ignore_index=True)'
-            if(right_answer.equals(expected_output)):
                 status_check[6]=1
                 isComplete = 1
-        elif questionName == 'Deleting Rows':#Q8
-            right_answer = default_df.drop(original_df.index[[4,9]])
-            right_answer_text = 'expected_output.drop(original_df.index[[4,9]])'
+        elif questionName == 'Dropping Columns and Rows':#Q7
+            right_answer = default_df.drop([4,9],axis=0)
+            right_answer_text = 'expected_output.drop([4,9],axis=0)'
             if(right_answer.equals(expected_output)):
                 status_check[7]=1
+                isComplete = 1
+        elif questionName == 'Sorting Values by Column':#Q8
+            right_answer = default_df.sort_values(by='AGE',ascending=False)
+            right_answer_text = 'expected_output.sort_values(by=\'AGE\',ascending=False)'
+            if(right_answer.equals(expected_output)):
+                status_check[8]=1
                 isComplete = 1
         elif questionName == 'Finding Min and Max':#Q9
             right_answer = default_df['NAME'].min()
             right_answer_text = 'expected_output[\'NAME\'].min()'
-            if(right_answer.equals(expected_output)):
-                status_check[8]=1
-                isComplete = 1
-        elif questionName == 'Multiplying and Dividing Column Values':#Q10
-            right_answer = 10*default_df.iloc[:,3]
-            right_answer_text = '10*expected_output.iloc[:,3]'
             if(right_answer.equals(expected_output)):
                 status_check[9]=1
                 isComplete = 1
         
         theMessage = ''
         if isComplete:
-            theMessage = "Yay! You got it right!"
+            theMessage = "Yay! You got it right! \n"
         else:
-            theMessage = "Incorrect. Please try again."
+            theMessage = "Incorrect. Please try again. \n"
         
         progress = status_check.count(1)
         print("Status Check", status_check)
@@ -168,7 +168,7 @@ def lambda_handler(event, context):
             "body":  json.dumps({
                 "isComplete":isComplete,
                 "pythonFeedback": log_contents.lower(),
-                "htmlFeedback": theMessage + "\n\n" + userHtmlFeedback,
+                "htmlFeedback": theMessage + userHtmlFeedback,
                 "textFeedback": right_answer_text,
                 "progress": progress,
                 "questionStatus":status_check
