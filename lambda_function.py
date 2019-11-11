f3c2b9bfc1e9536c90a17c2f11c714de1c784121
yi-luo <42324106+yi-luo@users.noreply.github.com>
2019-11-11 17:07:26 +0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,37 @@
+# http://alanwsmith.com/capturing-python-log-output-in-a-variable
+
 import json
 import pandas as pd
 import boto3 as bt3
 import re
+import os
+import logging
+import io
 
-status_check = [0]*10    
+logger = logging.getLogger('basic_logger')
+logger.setLevel(logging.DEBUG)
+
+status_check = [0]*10  
+
 def lambda_handler(event, context):
+    
+    ### Setup the console handler with a StringIO object
+    log_capture_string = io.StringIO()
+    ch = logging.StreamHandler(log_capture_string)
+    ch.setLevel(logging.DEBUG)
+    
+    ### Optionally add a formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    
+    ### Add the console handler to the logger
+    logger.addHandler(ch)
+    
+    def runCode(myDF, myCode):
+        namespace = {'original_df': myDF}
+        exec('import pandas as pd', namespace)
+        exec(myCode, namespace)
+        return namespace['original_df']
     
     s3 = bt3.client('s3')
     method = event.get('httpMethod',{}) 
@@ -33,27 +60,29 @@ def lambda_handler(event, context):
         original_df = pd.read_csv('https://frame-pandas.s3.amazonaws.com/pandas_data.csv')
         default_df = original_df.copy()
         
+        errorStatus = False
         # Evaluating User Inputs
-        user_list = userSolution.splitlines()
-        user_list = [x for x in user_list if not x.startswith('#')]
-        for i in user_list:
-            original_df = pd.eval(i)
-            
-        print("THIS IS STRING", original_df)
+        try:
+            original_df = runCode(original_df, userSolution)
+        except:
+            errorStatus = True
+            logger.exception('Debug Message')
         
-        if isinstance(original_df, str):
-            userHtmlFeedback = original_df
-        elif isinstance(original_df, pd.core.series.Series):
-           original_df = original_df.to_frame()
-           userHtmlFeedback = original_df.to_html()
+        if not errorStatus:
+            if isinstance(original_df, str):
+                userHtmlFeedback = original_df
+            elif isinstance(original_df, pd.core.series.Series):
+                original_df = original_df.to_frame()
+                userHtmlFeedback = original_df.to_html()
+            else:
+                userHtmlFeedback = original_df.to_html()
         else:
-            userHtmlFeedback = original_df.to_html()
+            userHtmlFeedback = "Error - Please Look at Python Logs"
         
         right_answer_text = "temp"
         isComplete = 0
         
-        
-        if questionName == 'Selecting Rows':#Q1
+        if questionName == 'Selecting Rows': #Q1
             right_answer = default_df.iloc[[1,4,9]]
             right_answer_text = 'original_df.iloc[[1,4,9]]'
             if(right_answer.equals(original_df)):
@@ -72,13 +101,13 @@ def lambda_handler(event, context):
                 status_check[2]=1
                 isComplete = 1
         elif questionName == 'Replacing String Occurrences':#Q4
-            right_answer = default_df.replace("USA","United States")
-            right_answer_text = 'original_df.replace("USA","United States")'
+            right_answer = default_df.replace(["F","M"],["Female","Male"])
+            right_answer_text = 'original_df.replace(["F","M"],["Female","Male"])'
             if(right_answer.equals(original_df)):
                 status_check[3]=1
                 isComplete = 1
         elif questionName == 'Filtering Data in Columns':#Q5
-            right_answer = default_df[original_df['CHILDREN']=='Yes']
+            right_answer = default_df[default_df['CHILDREN']=='Yes']
             right_answer_text = 'original_df[original_df[\'CHILDREN\']==\'Yes\']'
             if(right_answer.equals(original_df)):
                 status_check[4]=1
@@ -115,8 +144,13 @@ def lambda_handler(event, context):
                 isComplete = 1
         
         progress = status_check.count(1)
-        print(status_check)
-        print(progress)
+        print("Status Check", status_check)
+        print("Progress", progress)
+        
+        ### Pull the contents back into a string and close the stream
+        log_contents = log_capture_string.getvalue()
+        log_capture_string.close()
+        
         return {
             "statusCode": 200,
             "headers": {
@@ -124,7 +158,7 @@ def lambda_handler(event, context):
                 },
             "body":  json.dumps({
                 "isComplete":isComplete,
-                "pythonFeedback": "Hello",
+                "pythonFeedback": log_contents.lower(),
                 "htmlFeedback": userHtmlFeedback,
                 "textFeedback": right_answer_text,
                 "progress": progress,
